b3053a5bfb2fc8c5759e6f01b6fe5faccf97e7d0
sumitpatil21 <148967002+sumitpatil21@users.noreply.github.com>
2023-11-07 16:33:11 +0530

    Update que.c

--- SWITCH/que.c
+++ SWITCH/que.c
@@ -2,56 +2,47 @@
 
 int main()
 {
-		printf("1 : hindi\n");
-		printf("2 : english\n");
-		printf("3 : gujarati\n");
-		int l;
-		printf("choose your language :");
-		scanf("%d",&l);
-		
-
-		switch(l)
+	printf("1 : hindi\n");
+	printf("2 : english\n\n");
+	int en;
+	printf("enter your language?:");
+	scanf("%d",&en);
+	switch(en)
+	{
+		case 1 : printf("hindi\n\n");
+			int x;
+			printf("3 : Network error\n");
+			printf("4 : Call error\n");
+			printf("5 : Romeing error\n\n");
+			printf("aapki problem kya hai ? type to 3,4,5 :");
+			scanf("%d",&x);
+			
+		switch(x)
 		{
-				
-			case 1 : printf("hindi\n\n");
-		
-			printf("A : Network problem\n");
-			printf("B : call error\n");
-			printf("C : Recharge\n");
-			printf("D : Slow network\n\n ");
-			int enter;
-			printf("aapki problem kya hai ?");
-		    scanf("%d",&enter);
-			switch(enter);
-			{
-				printf("choise your problem ?");
-		   		 scanf("%d",&enter);
-				case 4 :printf("A : Network problem\n");
-			            printf("1 :Network issue\n");
-						printf("2 :Network slow \n");
-						printf("3 :Roaming problem\n\n");
-				switch(enter)
-				{
-					case 8 : printf("1 :Network issue\n");
-					case 9 : printf("2 :Network slow \n");
-					case 11: printf("3 :Roaming problem\n\n");
-				}
-				break;
-				case 5 :printf("B : call error");
-				break;
-				case 6 :printf("C : Recharge");
-				break;
-				case 7 :printf("D : Slow network ");
-				break;
-			
-			}
-			
-			
+			case 3 : printf("\nNetwork error");
 			break;
-			case 2 : printf("english");
+			case 4 : printf("\ncall error");
 			break;
-			case 3 : printf("gujarati");
+			case 5 : printf("\nRomeing error");
 			break;
+		}		
+	break;
+		case 2 : printf("english\n");
+			int y;
+			printf("6 : Network error\n");
+			printf("7 : Call error\n");
+			printf("8 : Romeing error\n\n");
+			printf("aapki problem kya hai ? type to 6,7,8 :");
+			scanf("%d",&y);
 			
+		switch(y)
+		{
+			case 6 : printf("\nNetwork error");
+			break;
+			case 7 : printf("\ncall error");
+			break;
+			case 8 : printf("\nRomeing error");
+			break;
 		}
+	}
 }
