2d181e4526728ea4375a9edd8777083119b44568
sumitpatil21 <148967002+sumitpatil21@users.noreply.github.com>
2023-11-08 12:09:45 +0530

    Add files via upload

---
+++ M/7-devi.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	int x;
+	printf("enter your value :");
+	scanf("%d",&x);
+	 if(x%7==0)
+	 {
+	 	printf("your value is divisible by 7 ");
+	 }
+	 else
+	 {
+	 	printf("your value is not divisible by 7 ");
+	 }
+}
