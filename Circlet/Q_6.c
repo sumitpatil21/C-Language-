2efd738c162a27610939e87c9b2eaec02f2ab1f7
sumitpatil21 <148967002+sumitpatil21@users.noreply.github.com>
2023-12-05 16:05:06 +0530

    Add files via upload

---
+++ Circlet/Q_6.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+main()
+{
+	int i ,j ,l,k;
+	for(i=5;i>=1;i--)
+	{
+			for(k=i;k>=1;k--)
+		{
+			printf(" ");
+		}
+		for(j=i;j<=5;j++)
+		{
+			printf("%d",j);
+		}
+		
+		for(l=4;l>=i;l--)
+		{
+			printf("%d",l);
+		}
+	
+	
+		printf("\n");
+	}
+}
